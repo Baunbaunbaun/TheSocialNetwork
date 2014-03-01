14191f813fbbe26d3ddc875947167076450602f5
Christian Baun <baunbaun@gmail.com>
2014-03-01 23:18:45 +0100

    _O/   \   /\_   \

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
-/**
- * Created by baunbaun on 01/03/14.
- */
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("YOLO");
+    public static void main(String[] args) {
+
+
+        String[] knud = new String[]{
+                "_O/   ",
+                "  \\   ",
+                "  /\\_ ",
+                "  \\   "};
+
+        String[] kasper = new String[]{
+                "   /  ",
+                " \\_\\  ",
+                "    \\ ",
+                "   /0\\"};
+
+        String[] ole = new String[]{
+                "  \\O_",
+                "/\\/  ",
+                " /   ",
+                " \\   "};
+
+        Photograph knudsPic = new Photograph(knud);
+
+//print picture
+        for (int i = 0; i < knud.length; i++) {
+            System.out.println(knudsPic.getLine(i));
+        }
+
     }
 }
---
+++ src/Photograph.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Photograph{
+
+    private String[] pic;
+
+    public Photograph(String[] photograph){
+
+        if (photograph == null){
+            photograph = new String[] { };
+        }
+        pic = photograph;
+    }
+
+    public int getWidth(){
+        return this.pic[0].length();
+    }
+
+    public int getHeight(){
+        return this.pic.length;
+    }
+
+    /**
+     * @null if the index is out of bounds.
+     */
+    public String getLine(int index){
+
+        if (index < 0 || index >= this.pic.length){
+            return null;
+        }
+    return pic[index];
+    }
+}
